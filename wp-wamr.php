aeb8f8829a97ba6165f9246da911d57cd56a0c8a
Namhyeon, Go <gnh1201@gmail.com>
2022-02-19 02:10:13 +0900

    Update wp-wamr.php

--- wp-wamr.php
+++ wp-wamr.php
@@ -213,7 +213,7 @@ function wp_media_load_wasm($filename) {
             }
         }
 
-        // For security reason, the media file must be compressed like '.wasm.zip'
+        // For security reason, the media file must be compressed like '*.wasm.zip'
         if (!empty($old_filepath) && file_exists($old_filepath)) {
             $zip = new ZipArchive();
             $result = $zip->open($old_filepath);
